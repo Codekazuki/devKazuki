40e407f75a6a423e9fe47c1dc2128d700fbf21ef
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-25 13:50:41 +0100

    portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className='nav-center'>
       <NavLink to='/'>
-        <span className='logo'>MixMaster</span>
+        <span className='logo'>CodeKazuki</span>
       </NavLink>
 
       <div className='nav-links'>
@@ -14,8 +14,11 @@ const Navbar = () => {
         <NavLink to='about' className='nav-link'>
           About
         </NavLink>
-        <NavLink to='newsletter' className='nav-link'>
-          Newsletter
+        <NavLink to='experience' className='nav-link'>
+          Experience
+        </NavLink>
+        <NavLink to='contactus' className='nav-link'>
+          Contact Us
         </NavLink>
       </div>
     </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 const About = () => {
   return (
-    <div>
+    <div className='about'>
       <h3>About Us</h3>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
