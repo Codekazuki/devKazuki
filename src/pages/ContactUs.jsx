6b1a99f169e0bdb674f10badfe98e44fc830d322
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-26 10:19:28 +0100

    portfolio

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,5 +1,40 @@
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <section className='contact'>
+      <form>
+        <h1>Contact Form</h1>
+        <div className='input-box'>
+          <label htmlFor=''>Full name</label>
+          <input
+            className='field'
+            type='text'
+            required
+            placeholder='yor name'
+          />
+        </div>
+        <div className='input-box'>
+          <label htmlFor=''>Email</label>
+          <input
+            className='field'
+            type='email'
+            required
+            placeholder='yor email'
+          />
+        </div>
+        <div className='input-box'>
+          <label htmlFor=''>Message</label>
+          <textarea
+            className='field message'
+            name=''
+            id=''
+            required
+            placeholder='your message'
+          ></textarea>
+        </div>
+        <button className='btn-submit'>Submit</button>
+      </form>
+    </section>
+  );
 };
 
 export default ContactUs;
