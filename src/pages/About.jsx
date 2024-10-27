9c9cffb150adc5237194e34a019a9a4e6378f971
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-27 02:28:12 +0100

    portfolio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,10 @@
+import image from "../assets/hello.svg";
 const intro =
   " I am Self-motivated Edtech professional and Web Developer with over a decade of teaching experience. Passionate about working on multiple projects and mentorship programmes, demonstrating a high level of commitment and dedication to education and technology.";
 const keyQual = "Key Qualifications";
+const prePoint1 = "Teaching Experience:";
 const point1 =
-  " Teaching Experience: Over a decade years of experience in educating students and professionals.";
+  "  Over a decade years of experience in educating students and professionals.";
 const point2 =
   "Web Development Expertise: Proficient in various web development languages, frameworks, and tools, with a track record of delivering high-quality projects.";
 const point3 =
@@ -14,19 +16,68 @@ const point5 =
 
 const About = () => {
   return (
-    <div className='about'>
+    <section className='about'>
       <h3>About Me</h3>
       <h3>{intro}</h3>
       <h2>{keyQual}:</h2>
-      <ul>
-        {/* add undraw images */}
-        <li>{point1}</li>
-        <li>{point2}</li>
-        <li>{point3}</li>
-        <li>{point4}</li>
-        <li>{point5}</li>
-      </ul>
-    </div>
+
+      {/* add undraw images */}
+      <section className='hero'>
+        <div className='hero-center'>
+          <div className='hero-title'>
+            <h2 className='pre-point'>{prePoint1}</h2>
+            <h3>{point1}</h3>
+          </div>
+          <div className='img-container'>
+            <img className='img' src={image} alt='' />
+          </div>
+        </div>
+      </section>
+      <section className='hero'>
+        <div className='hero-center'>
+          <div className='img-container'>
+            <img className='img' src={image} alt='' />
+          </div>
+          <div className='hero-title'>
+            <h2 className='pre-point'>{prePoint1}</h2>
+            <h3>{point1}</h3>
+          </div>
+        </div>
+      </section>
+      <section className='hero'>
+        <div className='hero-center'>
+          <div className='img-container'>
+            <img className='img' src={image} alt='' />
+          </div>
+          <div className='hero-title'>
+            <h2 className='pre-point'>{prePoint1}</h2>
+            <h3>{point1}</h3>
+          </div>
+        </div>
+      </section>
+      <section className='hero'>
+        <div className='hero-center'>
+          <div className='img-container'>
+            <img className='img' src={image} alt='' />
+          </div>
+          <div className='hero-title'>
+            <h2 className='pre-point'>{prePoint1}</h2>
+            <h3>{point1}</h3>
+          </div>
+        </div>
+      </section>
+      <section className='hero'>
+        <div className='hero-center'>
+          <div className='img-container'>
+            <img className='img' src={image} alt='' />
+          </div>
+          <div className='hero-title'>
+            <h2 className='pre-point'>{prePoint1}</h2>
+            <h3>{point1}</h3>
+          </div>
+        </div>
+      </section>
+    </section>
   );
 };
 
