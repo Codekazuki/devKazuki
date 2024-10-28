41bb261089dbb10bb66bb20295a81eda19fd2417
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-10-28 16:26:40 +0100

    portfolio

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,21 @@
+import resume from "../assets/resume.pdf";
+import { FaRegFileAlt } from "react-icons/fa";
 const Resume = () => {
-  return <div>Resume </div>;
+  return (
+    <div className='resume-div'>
+      <h2 className='resume-text'>
+        Click the button to view and download my resume
+      </h2>
+      <a rel='noopener noreferrer' href={resume} target='_blank'>
+        <button type='button' className=' resume-button '>
+          My Resume
+          <span style={{ padding: " 0 auto", fontSize: "2.2rem" }}>
+            <FaRegFileAlt />
+          </span>
+        </button>
+      </a>
+    </div>
+  );
 };
 
 export default Resume;
