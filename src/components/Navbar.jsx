6d2af90b3fa3e8cfd2465284e5819dd04569feba
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-12-26 22:55:04 +0100

    starting responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,37 +3,35 @@ import { FaRegFileAlt } from "react-icons/fa";
 import ThemeToggle from "./ThemeToggle";
 const Navbar = () => {
   return (
-    <>
-      <div className='nav-center'>
-        <div>
-          <NavLink to='/'>
-            <span className='logo'>CodeKazuki</span>
-          </NavLink>
-        </div>
-
-        <div className='nav-links'>
-          <NavLink to='/' className='nav-link'>
-            Home
-          </NavLink>
-          <NavLink to='about' className='nav-link'>
-            About
-          </NavLink>
-          <NavLink to='experience' className='nav-link'>
-            Experience
-          </NavLink>
-          <NavLink to='contactus' className='nav-link'>
-            Contact Us
-          </NavLink>
-          <NavLink to='resume' className='nav-link btn res'>
-            <span style={{ padding: " 0 auto", fontSize: "1.2rem" }}>
-              <FaRegFileAlt />
-            </span>
-            <span>Resume</span>
-          </NavLink>
-          <ThemeToggle />
-        </div>
+    <div className='nav-center'>
+      <div>
+        <NavLink to='/'>
+          <span className='logo'>CodeKazuki</span>
+        </NavLink>
       </div>
-    </>
+
+      <div className='nav-links'>
+        <NavLink to='/' className='nav-link'>
+          Home
+        </NavLink>
+        <NavLink to='about' className='nav-link'>
+          About
+        </NavLink>
+        <NavLink to='experience' className='nav-link'>
+          Experience
+        </NavLink>
+        <NavLink to='contactus' className='nav-link'>
+          Contact Us
+        </NavLink>
+        <NavLink to='resume' className='nav-link btn res'>
+          <span style={{ padding: " 0 auto", fontSize: "1.2rem" }}>
+            <FaRegFileAlt />
+          </span>
+          <span>Resume</span>
+        </NavLink>
+        <ThemeToggle />
+      </div>
+    </div>
   );
 };
 
