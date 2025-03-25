624146a9ced37094e8c16f9b730ab473a58a69d7
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-25 21:09:11 +0100

    sprint

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,19 +1,23 @@
 import resume from "../assets/resume.pdf";
 import { FaRegFileAlt } from "react-icons/fa";
 const Resume = () => {
+  let isButton = true;
+  console.log(isButton);
   return (
     <div className='resume-div'>
       <h2 className='resume-text'>
         Click the button to view and download my resume
       </h2>
-      <a rel='noopener noreferrer' href={resume} target='_blank'>
-        <button type='button' className=' resume-button '>
-          My Resume
-          <span style={{ padding: " 0 auto", fontSize: "2.2rem" }}>
-            <FaRegFileAlt />
-          </span>
-        </button>
-      </a>
+      {isButton && (
+        <a rel='noopener noreferrer' href={resume} target='_blank'>
+          <button type='button' className=' resume-button '>
+            My Resume
+            <span style={{ padding: " 0 auto", fontSize: "2.2rem" }}>
+              <FaRegFileAlt />
+            </span>
+          </button>
+        </a>
+      )}
     </div>
   );
 };
